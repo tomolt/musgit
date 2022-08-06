96a47df54734b4c51eee05855c1dac7fda5a66b9
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2022-08-06 14:13:31 +0200

    Opening the git repo.

--- musgit.c
+++ musgit.c
@@ -4,6 +4,7 @@
 #include <c-stache.h>
 #include <git2.h>
 
+static git_repository *gitRepo;
 static CStacheEngine stacheEngine;
 
 static void
@@ -25,17 +26,29 @@ make_log(const char *tplname)
 
 	status = c_stache_load_template(&stacheEngine, tplname, &tpl);
 	if (status != C_STACHE_OK)
-		die("cannot load template: %s", c_stache_strerror(status));
+		die("%s", c_stache_strerror(status));
 }
 
 int
 main()
 {
+	const git_error *error;
+	int status;
+
+	git_libgit2_init();
 	c_stache_start_engine(&stacheEngine, c_stache_read_file);
+
+	status = git_repository_open(&gitRepo, ".");
+	if (status < 0) {
+		error = git_error_last();
+		die("cannot open repo: %s", error->message);
+	}
 
 	make_log("log.html.mustache");
 
+	git_repository_free(gitRepo);
 	c_stache_shutdown_engine(&stacheEngine);
+	git_libgit2_shutdown();
 	return 0;
 }
 
